9f6f39334054af161ade538794783001ed10b9c7
ted wu <r05543056@g.ntu.edu.tw>
2024-02-17 11:31:03 +0900

    rollback to linebot version before 3

--- app.py
+++ app.py
@@ -1,73 +1,80 @@
+# import finlab
+# from finlab import data
 from flask import Flask, request, abort
+import requests, os
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageMessage, ImageSendMessage, FollowEvent, UnfollowEvent
+from io import BytesIO
+# import psycopg2
 
-from linebot.v3 import (
-     WebhookHandler
-)
-from linebot.v3.exceptions import (
-    InvalidSignatureError
-)
-from linebot.v3.messaging import (
-    Configuration,
-    ApiClient,
-    MessagingApi,
-    ReplyMessageRequest,
-    TextMessage
-)
-from linebot.v3.webhooks import (
-    MessageEvent,
-    TextMessageContent
-)
+# finlab.login('7SHZu9Y2jRV/TJ6outOrHEaZHKZiatjOO1joDGtwd8b42GNYOrA650bwvHlXqbyy#free')
+# data.get('price:收盤價')
 
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
-)
-import os
+
+LINE_CHANNEL_SECRET = "f2fe89950508f5bb231d88482d19d6b5"
+LINE_CHANNEL_ACCESS_TOKEN = "m05ZcFrzG9ojmIKaVvh9joYAcq0QYSIfWvjYILZ2m+ypIOhUXQtI98pHG4RSvX1YASaCp2defX5dAviryAb7YHW/OjYkIcyUloeQQImUVul8p/O0t030gwSl6zE5cxbLUHDmgS80xiAFSAGJswP/jAdB04t89/1O/w1cDnyilFU="
+# DATABASE_URL = "~~~~~あなたのデータベースURL~~~~~" # 後ほどHerokuでPostgreSQLデータベースURLを取得
+# HEROKU_APP_NAME = "~~~~~あなたのHerokuアプリ名~~~~~" # 後ほど作成するHerokuアプリ名
 
 app = Flask(__name__)
+# Heroku = "https://{}.herokuapp.com/".format(HEROKU_APP_NAME)
 
-#環境変数取得
-YOUR_CHANNEL_ACCESS_TOKEN = os.environ["CHANNEL_ACCESS_TOKEN"]
-YOUR_CHANNEL_SECRET = os.environ["CHANNEL_SECRET"]
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(LINE_CHANNEL_SECRET)
 
-configuration = Configuration(YOUR_CHANNEL_ACCESS_TOKEN)
-handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+# header = {
+#     "Content_Type": "application/json",
+#     "Authorization": "Bearer " + LINE_CHANNEL_ACCESS_TOKEN
+# }
+
 @app.route("/")
-def hello():
-    return "<h1> hello Test </h1>"
+def hello_world():
+    return "hello world!"
 
-@app.route("/callback", methods=['POST'])
+
+# アプリにPOSTがあったときの処理
+@app.route("/callback", methods=["POST"])
 def callback():
     # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
-
+    signature = request.headers["X-Line-Signature"]
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-
     # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
-        app.logger.info("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
+    return "OK"
 
-    return 'OK'
 
-
-@handler.add(MessageEvent, message=TextMessageContent)
+# botにメッセージを送ったときの処理
+@handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    with ApiClient(configuration) as api_client:
-        if event.message.text == "test":
-            msg = "Hello Tester"
-        else:
-            msg = "still under testing"
-        line_bot_api = MessagingApi(api_client)
-        line_bot_api.reply_message_with_http_info(
-            ReplyMessageRequest(
-                reply_token=event.reply_token,
-                messages=[TextMessage(msg)]
-            )
-        )
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
+    print("返信完了!!\ntext:", event.message.text)
+
+@handler.add(UnfollowEvent)
+def handle_unfollow(event):
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            conn.autocommit = True
+            cur.execute('DELETE FROM users WHERE user_id = %s', [event.source.user_id])
+    print("userIdの削除OK!!")
+
+# アプリの起動
+if __name__ == "__main__":
+    # 初回のみデータベースのテーブル作成
+    # with get_connection() as conn:
+    #     with conn.cursor() as cur:
+    #         conn.autocommit = True
+    #         cur.execute('CREATE TABLE IF NOT EXISTS users(user_id TEXT)')
+    
+    # LINE botをフォローしているアカウントのうちランダムで一人にプッシュ通知
+    # push()
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+### End
